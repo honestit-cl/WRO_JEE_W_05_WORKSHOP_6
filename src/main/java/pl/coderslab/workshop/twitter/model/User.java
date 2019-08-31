a475d2e37ddada7b5abdcaed29b8f883b18e7254
honestit <michal.kupisinski@gmail.com>
2019-08-31 16:52:30 +0200

    Przykład użycia TODO w IntelliJ

--- src/main/java/pl/coderslab/workshop/twitter/model/User.java
+++ src/main/java/pl/coderslab/workshop/twitter/model/User.java
@@ -10,6 +10,7 @@ public class User extends AbstractEntity {
     private String email;
     @Column(nullable = false)
     private String password;
+    // TODO Zapamiętaj, że na bazie jest inna konwencja nazewnicza i nie ma CamelCase
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
