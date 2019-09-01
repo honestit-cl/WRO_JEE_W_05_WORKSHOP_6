9690b8e51d631195b38933e1c2a1f7122bf6aaa9
honestit <michal.kupisinski@gmail.com>
2019-09-01 15:08:34 +0200

    Obsługa uwierzytelniania i logowania użytkownika z wykorzystaniem Spring Security

--- src/main/java/pl/coderslab/workshop/twitter/config/SecurityConfig.java
+++ src/main/java/pl/coderslab/workshop/twitter/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.coderslab.workshop.twitter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,9 +10,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/wro_jee_w_05_twitter");
+        dataSource.setUsername("root");
+//        dataSource.setPassword();
+        return dataSource;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -20,11 +33,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //
+        auth.jdbcAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .dataSource(dataSource())
+                .usersByUsernameQuery("SELECT email, password, true FROM users WHERE email = ?")
+                .authoritiesByUsernameQuery("SELECT email, 'ROLE_USER' FROM users WHERE email = ?");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //
+        http
+                .formLogin()
+                    .and()
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/register").permitAll()
+                    .antMatchers("/login").permitAll()
+                    .anyRequest().authenticated();
+
     }
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<%@ page isELIgnored="false" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Strona główha</title>
+</head>
+<body>
+<h1>Hurrrrrrraaaaa! Witaj ${username}</h1>
+</body>
+</html>
